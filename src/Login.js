46ba1465e6777c2939846e3094fdef0e5d71d436
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-21 17:14:26 +0100

    initial

---
+++ src/Login.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [input, setInput] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmitEvent = (e) => {
+    e.preventDefault();
+    if (input.username !== "" && input.password !== "") {
+      //dispatch action from hooks
+    }
+    alert("please provide a valid input");
+  };
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  return (
+    <form onSubmit={handleSubmitEvent}>
+      <div className="form-control">
+        <label htmlFor="user-email">Email:</label>
+        <input
+          className="form-control"
+          type="email"
+          id="user-email"
+          name="email"
+          placeholder="example@yahoo.com"
+          aria-describedby="user-email"
+          aria-invalid="false"
+          onChange={handleInput}
+        />
+        <div id="user-email" className="sr-only">
+          Please enter a valid username. It must contain at least 6 characters.
+        </div>
+      </div>
+      <div className="form-control">
+        <label htmlFor="password">Password:</label>
+        <input
+          className="form-control"
+          type="password"
+          id="password"
+          name="password"
+          aria-describedby="user-password"
+          aria-invalid="false"
+          onChange={handleInput}
+        />
+        <div id="user-password" className="sr-only">
+          your password should be more than 6 character
+        </div>
+      </div>
+      <div className="form-control">
+        <button className="btn btn-primary btn-submit form-control">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+}
