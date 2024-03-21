b59b7b92c392e7615b73484b6600727d60bfc513
rod7773 <rodrigoarcadio1@gmail.com>
2024-03-21 17:25:11 +0100

    update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuth } from "../hooks/AuthProvider";
 
-export default function Dashboard() {
-  return <div>Dashboard</div>;
-}
+const Dashboard = () => {
+  const auth = useAuth();
+  return (
+    <div className="container">
+      <div>
+        <h1>Welcome! {auth.user?.username}</h1>
+        <button onClick={() => auth.logOut()} className="btn-submit">
+          logout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
